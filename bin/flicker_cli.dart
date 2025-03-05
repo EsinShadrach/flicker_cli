924c45c2171dfda72c14e1a9d12df841bcdad886
EsinShadrach <esinshadrachrafe@gmail.com>
2025-03-05 21:44:21 +0100

    initial commit

---
+++ bin/flicker_cli.dart
@@ -0,0 +1,5 @@
+import 'package:flicker_cli/flicker_cli.dart' as flicker_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flicker_cli.calculate()}!');
+}
---
+++ test/flicker_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:flicker_cli/flicker_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
